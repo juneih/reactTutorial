2cab217320b75986c2cb39b719310d7573a4cd8e
June Henriksen <junehenriksen@hotmail.com>
2016-04-15 13:34:46 +0200

    show last twitter message

--- src/index.js
+++ src/index.js
@@ -1,30 +1,41 @@
 import {createStore} from 'redux';
 import React from 'react';
 import { render } from 'react-dom';
+import  Tweet from './components/Tweet.jsx';
+import MyTweet from './example-tweet';
 
-function reducer (counter=0, action){
-	if (action.type === 'USER_CLICKED'){
-		counter++;
-		console.log(counter);
+function reducer (state={tweets: [MyTweet]}, action){
+	if (action.type === 'TWEET_REEIVED'){
+		state = {...state, tweets: [action.newTweet].concat(state.tweets) };
+		console.log('test');
 	}
-	return counter;
+
+	return state;
 }
 
 let store = createStore(reducer);
 
 
-document.addEventListener('click', () => store.dispatch({ type: 'USER_CLICKED' }));
+const ws = new WebSocket('ws://twitterws.herokuapp.com');
+const MAX_TWEETS = 1000;
+
+ws.onmessage = ms => {
+  const tweet = JSON.parse(ms.data);
+  if (store.getState().tweets.length < MAX_TWEETS) {
+	// add new tweet here by dispatching action
+	store.dispatch({ type: 'TWEET_REEIVED', newTweet: tweet});
+  }
+};
+
+//document.addEventListener('click', () => store.dispatch({ type: 'TWEET_REEIVED', tweet: MyTweet}));
 
 
 
-function Counter(props) {
-  return <div>{props.counter}</div>;
+let TweetComponent = () => {
+	render(<Tweet tweet={store.getState().tweets.pop()} />, document.querySelector('#app'));
 }
 
-let MyApp = () => {
-	render(<Counter counter={store.getState()} />, document.querySelector('#app'));
-}
+store.subscribe(TweetComponent, document.querySelector('#app'));
 
-store.subscribe(MyApp, document.querySelector('#app'));
+//TweetComponent({tweet: MyTweet});
 
-MyApp();
