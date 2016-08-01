8ff051423e3e3058ddb438753b8c1e33427e642c
June Henriksen <juneih@redpill-linpro.com>
2016-08-01 20:31:13 +0200

    Render different views based on route. Finishes task 5.

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,6 +5,12 @@ import Link from '../containers/Link.jsx';
 
 let App =  (props) => {
 
+    var content = <div>Whaaaaat!</div>;
+    console.log('props.route is', props.route);
+    if ( props.route == "/feed" ){
+        content = <TweetFeed tweets={props.tweets} />;
+    }
+
     return (<div className="app-header">
                 <div>
                     <h1 className="heading">Twitter redux stuff</h1>
@@ -12,8 +18,8 @@ let App =  (props) => {
                         <Link to="/" className="img img-icon img-icon-dashboard"/>
                         <Link to="/feed" className="img img-icon img-icon-settings"/>
                     </div>
+                    {content}
                 </div>
-                <TweetFeed tweets={props.tweets} />
             </div>
            );
 }
@@ -21,7 +27,8 @@ let App =  (props) => {
 let mapStateToProps = (state)=> {
     console.log('mapstatetoprops state in app.jsx',state);
     return {
-        tweets: state.tweets
+        tweets: state.tweets,
+        route: state.route
     }
 }
 export default connect(mapStateToProps)(App);
